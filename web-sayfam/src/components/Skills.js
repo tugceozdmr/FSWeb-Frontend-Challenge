4c9f8a9320b50c50ae729c1dd3f1b19658066d96
tugce <tugceozdemiir@hotmail.com>
2023-02-08 20:40:47 +0300

    tamamlandı

---
+++ web-sayfam/src/components/Skills.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function Skills() {
+  return (
+    <div className="skl">
+      
+      <h2>
+        Skills
+        <div className="skl2">
+          <p>Html</p>
+          <p>Css</p>
+          <p>JavaScript</p>
+          <p>React</p>
+        </div>
+      </h2>
+    </div>
+  );
+}
